6f71ee9fdf10b381e6a22d72b9a5c438e60db88f
Alex14460 <134074331+Alex14460@users.noreply.github.com>
2026-07-13 17:25:03 +0200

    Configure site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexandreroussel.com",
+  site: "https://www.alexandreroussel.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
